7134e5a9658d162e12da8dbfc43cf2cf7105273f
Suriy prakash <uriyaprakash@gmail.com>
2025-03-20 06:49:19 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css'
@@ -12,11 +12,11 @@ createRoot(document.getElementById('root')).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
-  <BrowserRouter basename="/nova-school/">
+  <HashRouter basename="/nova-school/">
     <Routes>
       <Route path='/app' element={<App />} />
       <Route path='/' element={<Home />} />
       <Route path='/faculty' element={<Faculty />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
